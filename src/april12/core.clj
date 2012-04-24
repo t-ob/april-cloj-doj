e8e5e5079580ec911f14c845e742d27cfa203941
Tom O'Brien <thomas.obrien86@gmail.com>
2012-04-24 15:06:36 +0100

    Initial commit

---
+++ src/april12/core.clj
@@ -0,0 +1,1 @@
+(ns april12.core)
---
+++ test/april12/test/core.clj
@@ -0,0 +1,6 @@
+(ns april12.test.core
+  (:use [april12.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
